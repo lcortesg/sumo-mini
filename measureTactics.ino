a1eeda85b11b01273b0a412053f33ddaf9678d5b
Lucas Felipe Cortés Gutiérrez <lcortes@alumnos.inf.utfsm.cl>
2019-12-19 15:20:31 -0300

    Upload New File

---
+++ measureTactics.ino
@@ -0,0 +1,23 @@
+/**
+ * @function measureLine : Lee los sensores de línea y determina si se está detecctando un fondo blanco
+ * @param : none
+ * @return : retorna 0 si no se detecta una línea, 1 en caso contrario
+ */
+
+
+bool measureEnemy(){
+    enemy_sensor_left = !digitalRead(PIN_SENSOR_LEFT);
+    enemy_sensor_front = !digitalRead(PIN_SENSOR_FRONT);
+    enemy_sensor_right = !digitalRead(PIN_SENSOR_RIGHT);
+
+    #if DEBUG > NONE
+        Serial.print("\n>>> LOG: mediciones de enemigo: \t");
+        Serial.print(enemy_sensor_left);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_front);
+        Serial.print("\t");
+        Serial.print(enemy_sensor_right);
+    #endif
+        
+    return (enemy_sensor_left) || (enemy_sensor_front) || (enemy_sensor_right);
+}
