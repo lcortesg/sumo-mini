ab13c6258964a45aaef398d24d63e76c8da8eb8f
Lucas Cortés <lucas.cortes.14@sansano.usm.cl>
2020-04-10 13:26:26 -0400

    typo

--- MeasureTactics.ino
+++ MeasureTactics.ino
@@ -1,13 +1,5 @@
 /**
-<<<<<<< HEAD
-<<<<<<< HEAD
  * @function measureLine : Lee los sensores de línea y determina si se está detectando un fondo blanco
-=======
- * @function measureLine : Lee los sensores de línea y determina si se está detectando un fondo blanco 
->>>>>>> 78b7154f0c3b81b701fbb7887594625cef9d480f
-=======
- * @function measureLine : Lee los sensores de línea y determina si se está detectando un fondo blanco 
->>>>>>> 7cbec1333e3d31a5b8b6a58d6d2d1589a6fec0e3
  * @param : none
  * @return : retorna 0 si no se detecta una línea, 1 en caso contrario
  */
